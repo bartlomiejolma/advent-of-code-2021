e79b5640335c386f67f397ffd4d2b418f7850943
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-01 10:05:42 +0100

    day1: part 2

--- day-1/src/main/scala/Main.scala
+++ day-1/src/main/scala/Main.scala
@@ -2,8 +2,19 @@ import scala.io.Source
 
 val filename = "input.txt"
 
-@main def hello: Unit = 
+def partOne: Unit = 
   def text = Source.fromFile(filename).getLines
   def increaseCount = text.sliding(2).count(pair => pair(1).toInt > pair(0).toInt) 
   println(increaseCount)
 
+def partTwo: Unit = 
+  def text = Source.fromFile(filename).getLines
+  def size = 3
+  def averages = text.sliding(size).map(subset => subset.map(_.toInt).sum) 
+  def increaseCount = averages.sliding(2).count(pair => pair(1) > pair(0)) 
+
+  println(increaseCount)
+
+@main def hello: Unit = 
+  partOne
+  partTwo
