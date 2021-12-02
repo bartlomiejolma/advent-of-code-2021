c91485412751144edf2610fa7b53790fcde1f275
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-02 09:42:27 +0100

    day2: part 2

--- day-2/src/main/scala/Main.scala
+++ day-2/src/main/scala/Main.scala
@@ -12,15 +12,33 @@ def move1(text: List[String]): (Int, Int) =
       case s"down $distance" => (0, distance.toInt)
     }
   ).fold(initialPosition)((acc, x) => (acc._1 + x._1, acc._2 + x._2))
-  println(position)
+  // println(position)
   position 
 
+
+def move2(text: List[String]): (Int, Int, Int) = 
+  val initialPosition = (0,0,0)
+
+  val position = text.map(
+    instruction => instruction match {
+      case s"forward $distance" => (distance.toInt, 0)
+      case s"up $distance" => (0, -distance.toInt)
+      case s"down $distance" => (0, distance.toInt)
+    }
+  ).foldLeft(initialPosition)((current, command) => (current._1 + command._1, current._2 + command._1 * current._3, current._3 + command._2))
+  println(position)
+  position 
 
 def partOne: Unit = 
   def text = Source.fromFile(filename).getLines.toList
   val (horizontal, vertical) = move1(text)
   println(horizontal * vertical)
 
+def partTwo: Unit = 
+  def text = Source.fromFile(filename).getLines.toList
+  val (horizontal, vertical, _) = move2(text)
+  println(horizontal * vertical)
 
 @main def hello: Unit = 
   partOne
+  partTwo
--- day-2/src/test/scala/Test1.scala
+++ day-2/src/test/scala/Test1.scala
@@ -1,17 +1,34 @@
 import org.junit.Test
 import org.junit.Assert.*
 
+
+val sampleInput = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2""".split("\n").toList
+
 class Test1:
   @Test def t1(): Unit = 
     def text = List("forward 1", "up 1", "forward 1")
     assertEquals((2, -1), move1(text))
 
   @Test def tsample(): Unit = 
-    def text = """forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2"""
-    assertEquals((15, 10), move1(text.split("\n").toList))
+    def text = sampleInput
+    assertEquals((15, 10), move1(text))
 
+
+
+class Test2:
+  @Test def t1(): Unit = 
+    def text = List("forward 1", "up 1", "forward 1")
+    val (horizontal, vertical, _ ) = move2(text)
+    assertEquals(2, horizontal)
+    assertEquals(-1, vertical)
+
+  @Test def tsample(): Unit = 
+    def text = sampleInput
+    val (horizontal, vertical, _ ) = move2(text)
+    assertEquals(15, horizontal)
+    assertEquals(60, vertical)
