f4310da8a1d78bbf8b651de509e917f846a475fd
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-02 09:14:43 +0100

    day2: part1

--- day-2/src/main/scala/Main.scala
+++ day-2/src/main/scala/Main.scala
@@ -1,5 +1,26 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
 
-def msg = "I was compiled by Scala 3. :)"
+val filename = "input.txt"
+
+def move1(text: List[String]): (Int, Int) = 
+  def initialPosition = (0,0)
+
+  val position = text.map(
+    instruction => instruction match {
+      case s"forward $distance" => (distance.toInt, 0)
+      case s"up $distance" => (0, -distance.toInt)
+      case s"down $distance" => (0, distance.toInt)
+    }
+  ).fold(initialPosition)((acc, x) => (acc._1 + x._1, acc._2 + x._2))
+  println(position)
+  position 
+
+
+def partOne: Unit = 
+  def text = Source.fromFile(filename).getLines.toList
+  val (horizontal, vertical) = move1(text)
+  println(horizontal * vertical)
+
+
+@main def hello: Unit = 
+  partOne
--- day-2/src/test/scala/Test1.scala
+++ day-2/src/test/scala/Test1.scala
@@ -3,4 +3,15 @@ import org.junit.Assert.*
 
 class Test1:
   @Test def t1(): Unit = 
-    assertEquals("I was compiled by Scala 3. :)", msg)
+    def text = List("forward 1", "up 1", "forward 1")
+    assertEquals((2, -1), move1(text))
+
+  @Test def tsample(): Unit = 
+    def text = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
+    assertEquals((15, 10), move1(text.split("\n").toList))
+
