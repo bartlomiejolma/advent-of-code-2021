053e93b3e80ef9a27be8ab30080b11fc43c5bec8
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-08 10:46:40 +0100

    da4: refactroing and part2

--- day-4/src/main/scala/Main.scala
+++ day-4/src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 
 val filename = "input.txt"
 
-case class Board(board: Array[Array[Int]]) {
+case class Board(board: Array[Array[Int]], var won: Boolean = false)  {
   def mkString(): String = {
     board.map(_.mkString(" ")).mkString("\n")
   }
@@ -20,14 +20,17 @@ def parseInput(input: String): Input = {
   Input(numbers, boards)
 }
 
- 
-def partOne(text: String): Int = 
+type BoardIndex = (Int, Int, Int)
+type BoardWinIndex = (Int, Int)
 
-  val input = parseInput(text)
+type NumbersPositions = scala.collection.mutable.Map[Int, List[BoardIndex]]
+type Counts = scala.collection.mutable.Map[BoardWinIndex, Int]
 
-  val numbersPositions = scala.collection.mutable.Map[Int, List[Tuple3[Int, Int, Int]]]()
-  val countsRow = scala.collection.mutable.Map[Tuple2[Int, Int], Int]()
-  val countsCol = scala.collection.mutable.Map[Tuple2[Int, Int], Int]()
+def getDataStructures(input: Input): Tuple3[NumbersPositions, Counts, Counts] = {
+
+  val numbersPositions: NumbersPositions = scala.collection.mutable.Map[Int, List[BoardIndex]]()
+  val countsRow = scala.collection.mutable.Map[BoardWinIndex, Int]()
+  val countsCol = scala.collection.mutable.Map[BoardWinIndex, Int]()
 
   for (case (board, boardIndex) <- input.boards.zipWithIndex) {
     for (case (row, rowIndex) <- board.board.zipWithIndex) {
@@ -41,27 +44,68 @@ def partOne(text: String): Int =
       }
     }
   }
+  return (numbersPositions, countsRow, countsCol)
+}
+
+def partOneWon(boards: Array[Board], boardIndex:BoardIndex): Boolean = {
+    println(s"$boardIndex board won by row")
+    println(boards(boardIndex._1).mkString())
+    return true
+}
+
+def partTwoWon(boards: Array[Board], boardIndex:BoardIndex): Boolean = {
+    boards(boardIndex._1).won = true
+    if (boards.forall(board => board.won)) {
+      println(s"$boardIndex board won by col")
+      println(boards(boardIndex._1).mkString())
+      return true
+    } 
+    return false
+}
+
+type WonHandler = (Array[Board], BoardIndex) => Boolean
+
+
+def runBingo (wonHandler: WonHandler)(input: Input, numbersPositions: NumbersPositions, countsRow: Counts, countsCol: Counts): Tuple2[Board, Int] = {
+
   for (number <- input.numbers) {
     val positions = numbersPositions(number)
     for (position <- positions) {
       countsRow(Tuple2(position._1, position._2)) += 1
       countsCol(Tuple2(position._1, position._3)) += 1
       input.boards(position._1).board(position._2)(position._3) = 0
-      if (countsRow(Tuple2(position._1, position._2)) == 5) {
-        println(s"$position board won by row")
-        println(input.boards(position._1).mkString())
-        return number * input.boards(position._1).sum()
-      }
-      if (countsCol(Tuple2(position._1, position._3)) == 5) {
-        println(s"$position board won by col")
-        println(input.boards(position._1).mkString())
-        return number * input.boards(position._1).sum()
-
+      val boardWon = countsRow(Tuple2(position._1, position._2)) == 5 || countsCol(Tuple2(position._1, position._3)) == 5
+      val shouldFinish = boardWon && wonHandler(input.boards, position)
+      if (shouldFinish) {
+        return (input.boards(position._1), number)
       }
     }
   }
-  return 0
+  return (input.boards(0), -1)
+}
+
+
+def getResult(winner: (Board, Int) ): Int = {
+  return winner._2 * winner._1.sum()
+}
+
+def partOne(text: String): Int = 
+
+  val input = parseInput(text)
+
+  val (numbersPositions, countsRow, countsCol) = getDataStructures(input)
+    
+  return getResult(runBingo(partOneWon)(input, numbersPositions, countsRow, countsCol))
+
+def partTwo(text: String): Int = 
+
+  val input = parseInput(text)
+
+  val (numbersPositions, countsRow, countsCol) = getDataStructures(input)
+    
+  return getResult(runBingo(partTwoWon)(input, numbersPositions, countsRow, countsCol))
 
 @main def hello: Unit = 
   def text = Source.fromFile(filename).mkString
   println(partOne(text))
+  println(partTwo(text))
--- day-4/src/test/scala/Test1.scala
+++ day-4/src/test/scala/Test1.scala
@@ -24,3 +24,6 @@ val input = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3
 class Test1:
   @Test def t1(): Unit = 
     assertEquals(4512, partOne(input))
+
+  @Test def t2(): Unit = 
+    assertEquals(1924, partTwo(input))
