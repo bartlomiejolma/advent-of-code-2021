e767a79872a9bd9c1eb7d8fa32e1d0df6dde5e0f
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-16 21:20:18 +0100

    day10: part1

---
+++ day-12/src/main/scala/Main.scala
@@ -0,0 +1,47 @@
+import scala.io.Source
+
+val filename = "input.txt"
+
+def parseLine(line: String): (String, String) = {
+  val parts = line.split("-")
+  (parts(0), parts(1))
+}
+
+type Nodes = Map[String, Array[String]]
+def parseInput(text: String): Nodes = 
+  val lines = text.split("\n")
+  lines.map(parseLine)
+  .map(t => List((t._1, t._2),(t._2, t._1)))
+  .flatten
+  .groupBy(_._1)
+  .map((kv) => (kv._1, kv._2.map(_._2)))
+  .toMap
+
+
+def findStart(nodes: Nodes): String = {
+  val next = nodes.map(_._2).flatten.toSet
+  val all = nodes.keys.toSet
+  (all -- next).head
+}
+
+
+def search(nodes:Nodes, way: List[String], current: String): Array[List[String]] =
+  println(way.mkString(" <- "))
+
+  println(current)
+  current match {
+    case "end" => Array(current :: way)
+    case s"$next" => nodes(current)
+      .filter(a_next => !way.contains(a_next) || a_next == a_next.toUpperCase)
+      .flatMap(a_next => search(nodes, current :: way, a_next))
+  }
+
+
+def partOne(text: String): Int = {
+  val ways = parseInput(text)
+  ways.foreach(kv => println(kv._1 + " -> " + kv._2.mkString(", ")))
+  search(ways, List(), "start").length
+}
+@main def hello: Unit = 
+  val text = Source.fromFile(filename).mkString
+  println(partOne(text))
---
+++ day-12/src/test/scala/Test1.scala
@@ -0,0 +1,16 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+
+val input = """start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"""
+
+
+class Test1:
+  @Test def t1(): Unit = 
+    assertEquals(10, partOne(input))
