8013b1b8991a8522c24ea8f6f088624420bdd34d
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-16 22:26:08 +0100

    day 10: part 2

--- day-10/src/main/scala/Main.scala
+++ day-10/src/main/scala/Main.scala
@@ -14,6 +14,14 @@ val scores = Map[Char, Int](
   '}' -> 1197,
   '>' -> 25137
 )
+
+val autocompleteScores = Map[Char, Int](
+  ')' -> 1,
+  ']' -> 2,
+  '}' -> 3,
+  '>' -> 4
+)
+
 def parseLine(expected: Option[Char] = None)(line: String): (Option[Char], String) = 
 
   line match {
@@ -33,13 +41,44 @@ def parseLine(expected: Option[Char] = None)(line: String): (Option[Char], Strin
       }
     }
 
+def autocompleteLine(expected: List[Char])(line: String): List[Char] = 
+
+  // println(s"autocompleteLine($expected)($line)")
+  line match {
+    case "" => expected
+    case _ => {
+
+      expected match {
+        case e::rest if e == line.head => autocompleteLine(rest)(line.tail)
+        case _ =>  autocompleteLine(expectedNext(line.head) :: expected)(line.tail)
+        }
+      }
+    }
+
+
 def partOne(text: String): Int = {
 
     text.split('\n').map(x => parseLine(None)(x)._1).flatten.map(scores).sum
 }
 
 
+def partTwo(text: String): Long = {
+
+    val lines = text.split('\n')
+    val scores =
+    lines.map(x => parseLine(None)(x))
+    .zip(lines)
+    .filter(x => !x._1._1.isDefined).map(_._2)
+    .map(autocompleteLine(List[Char]()))
+    .map(
+      x => x.map(autocompleteScores)
+            .foldLeft(0L)((acc, score) => acc * 5 + score)
+    )
+    .sortWith(_ > _)
+    scores(scores.length  / 2)
+}
 @main def hello: Unit = 
   val text = Source.fromFile(filename).mkString
   println(partOne(text))
+  println(partTwo(text))
 
--- day-10/src/test/scala/Test1.scala
+++ day-10/src/test/scala/Test1.scala
@@ -17,3 +17,8 @@ val input = """[({(<(())[]>[[{[]{<()<>>
 class Test1:
   @Test def t1(): Unit = 
     assertEquals(26397, partOne(input))
+ 
+  @Test def autocomplete(): Unit = 
+    assertEquals("}}]])})]", autocompleteLine(List[Char]())("[({(<(())[]>[[{[]{<()<>>").mkString)
+  @Test def t2(): Unit = 
+    assertEquals(288957, partTwo(input))
