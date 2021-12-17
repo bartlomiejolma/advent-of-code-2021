5a09f04daf6e8c47a4657897686caddca7f65e3a
bartlomiej.olma.kitopi <bartlomiej.olma@kitopi.com>
2021-12-17 12:44:36 +0100

    day 9: part 2

--- day-9/src/main/scala/Main.scala
+++ day-9/src/main/scala/Main.scala
@@ -38,6 +38,44 @@ def partOne(text: String): Int = {
   lowpoints.flatten.sum
 }
 
+type Visited = scala.collection.mutable.Map[(Int, Int), (Int, Int)]
+
+def go(heightMap:HeightMap)(start: (Int, Int))(visited: Visited, x: Int, y: Int): Unit = 
+
+  val height = heightMap.get(x)(y)
+  // println(s"$x, $y $height $start $visited")
+    visited.get((x,y)) match {
+      case None =>
+        height match {
+          case Some(9) => visited((x,y)) = (-1, -1)
+          case None => visited((x,y)) = (-1, -1)
+          case Some(validHeight) => 
+            visited((x,y)) = start
+            go(heightMap)(start)(visited, x+1, y)
+            go(heightMap)(start)(visited, x-1, y)
+            go(heightMap)(start)(visited, x, y - 1)
+            go(heightMap)(start)(visited, x, y + 1)
+        }
+      case Some(start) =>
+    }
+  
+def partTwo(text: String): Int = 
+  val heightMap = parseInput(text)
+  val visited = scala.collection.mutable.Map[(Int, Int), (Int, Int)]()
+  val lowpoints = heightMap.data.zipWithIndex
+  .foreach { case (row, x) => 
+      row.zipWithIndex.map { case (height, y) =>  
+        go(heightMap)((x,y))(visited, x, y)
+      }
+    }
+
+  val basins = visited.filter(e => e._2._1 != -1).groupBy(_._2)
+  println(basins)
+
+  basins.map(e => e._2.size).toArray.sortWith(_ > _).zip(1 to 3).map(_._1).reduce(_ * _)
+
+  
 @main def hello: Unit = 
   val text = Source.fromFile(filename).mkString
   println(partOne(text))
+  println(partTwo(text))
